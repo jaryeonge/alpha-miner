0c41e6d3cb27d70b7109b7f05a5f7e3d58b3b946
jaryeonge <jaryeonge@gmail.com>
2023-11-14 10:58:51 +0900

    [issue#2] Add a 'unit' variable to the function for getting candle data

--- src/data/crud/candle_minute_crud.py
+++ src/data/crud/candle_minute_crud.py
@@ -4,9 +4,9 @@ from sqlalchemy.orm import Session
 from ..models import CandleMinute
 
 
-def get_all_candle_minute(db: Session, market: str):
-    return db.query(CandleMinute).filter(CandleMinute.market == market)
+def get_all_candle_minute(db: Session, market: str, unit: int):
+    return db.query(CandleMinute).filter(CandleMinute.market == market).filter(CandleMinute.unit == unit)
 
 
-def get_candle_minute_by_kst(db: Session, market: str, start_kst: datetime, end_kst: datetime):
-    return db.query(CandleMinute).filter(CandleMinute.market == market).filter(CandleMinute.candle_date_time_kst >= start_kst).filter(CandleMinute.candle_date_time_kst <= end_kst)
+def get_candle_minute_by_kst(db: Session, market: str, unit:int, start_kst: datetime, end_kst: datetime):
+    return db.query(CandleMinute).filter(CandleMinute.market == market).filter(CandleMinute.unit == unit).filter(CandleMinute.candle_date_time_kst >= start_kst).filter(CandleMinute.candle_date_time_kst <= end_kst)
